55346d70322fde3782c688418a849f83bf00261b
Daniel Wells <wellsd3@wwu.edu>
2021-04-15 10:57:37 -0700

    dan did problems 1- halfway through 5

---
+++ lab2/lab2.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(define (permutations size)
+  (let ((elements (list #t #f)))
+    (if (zero? size)
+        '(())
+        (append-map (lambda (p)
+                      (map (lambda (e) (cons e p)) elements))
+                    (permutations (sub1 size))))))
+
+(define (implies p q)
+  (or (not p) q))
+
+(define (expr1 p q r)
+  (implies p (implies q r)))
+
+(define (expr2 p q r)
+  (implies q (implies p r)))
+
+(define (expr3 p q r)
+  (implies r (implies q p)))
+
+;q -> ~p
+(define (expr4 p q)
+  (implies q (not p)))
+
+;~p ^ q
+(define (expr5 p q)
+  (and (not p) q))
+
+;(~p V q) -> r
+(define (expr6 p q r)
+  (implies (or (not p) q) r))
+
+;((r ^ (q -> (~p))) -> p)
+(define (expr7 p q r)
+  (implies (and r (implies q (not p))) p))
